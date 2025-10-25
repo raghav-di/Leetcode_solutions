f5ad83f8d405543497aaf15077f745a2eb2f8f1d
Raghav Dhyani <raghavdhyaniwork15@gmail.com>
2025-10-25 21:35:08 +0530

    Time: 0 ms (100%), Space: 43.7 MB (5.94%) - LeetHub

---
+++ 2621-find-xor-beauty-of-array/2621-find-xor-beauty-of-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int xorBeauty(vector<int>& nums) {
+        int num = 0;
+        for(int e: nums){
+            num = num^e;
+        }
+        return num^0;
+    }
+};
