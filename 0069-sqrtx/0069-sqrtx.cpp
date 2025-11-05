25d6272be52405797de6cda48f8d84fff268e42e
Raghav Dhyani <raghavdhyaniwork15@gmail.com>
2025-11-05 13:30:53 +0530

    Time: 1 ms (23.2%), Space: 8.6 MB (14.95%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x < 2) return x;
-
-        int count = 0;
-        int temp = x;
-        while (temp > 0) {
+        if(x==0){
+            return 0;
+        }
+        if(x==1){
+            return 1;
+        }
+        int count = 0, temp = x;
+        while(temp>0){
             count++;
             temp >>= 1;
         }
-
-        int result = 0;
-        for (int i = (count + 1) / 2; i >= 0; --i) {
-            result |= (1 << i);
-            if ((long long) result * result > x) {
-                result ^= (1 << i);
+        int res = 0;
+        for(int i = (count)/2; i>=0; i--){
+            res |= 1<<i;
+            if((long long)res*res>x){
+                res ^= (1<<i);
             }
         }
-
-        return result;       
+        return res;
     }
 };
