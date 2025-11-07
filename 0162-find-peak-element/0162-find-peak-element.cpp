75b387e9249bf5693497f02b230b73b091f069ad
Raghav Dhyani <raghavdhyaniwork15@gmail.com>
2025-11-07 15:32:32 +0530

    Time: 0 ms (100%), Space: 12.4 MB (69.01%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -8,22 +8,18 @@ public:
                 idx = (res<nums[s])? s:idx;
                 res = (res<nums[s])? nums[s]:res;
                 e = m-1;
-                cout<<s<<"s ";
             }
             else if(nums[m]>=nums[s]&&nums[m]>=nums[e]){
                 idx = (res<nums[m])? m:idx;
                 res = (res<nums[m])? nums[m]:res;
                 s++;
                 e--;
-                cout<<m<<"m ";
             }
             else if(nums[e]>=nums[m]&&nums[e]>=nums[s]){
                 idx = (res<nums[e])? e:idx;
                 res = (res<nums[e])? nums[e]:res;
                 s = m+1;
-                cout<<e<<"e ";
             }
-            cout<<idx<<"idx ";
         }
         return idx;
     }
